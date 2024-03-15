47291583a6299eba1ad47f338e9ae3cb02955e40
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-03-15 16:31:50 -0500

    feat: wrap logo and cart icon in Link components
    This commit wraps the logo and shopping cart icon within the Header component in Link components. This enables the users to navigate back to the home page when they click on the logo and forward to the checkout page when they click on the shopping cart icon.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,11 +6,14 @@ import {
 } from "./styles.ts";
 import logo from "../../assets/img/logo.svg";
 import { MapPin, ShoppingCart } from "@phosphor-icons/react";
+import { Link } from "react-router-dom";
 
 export function Header() {
   return (
     <Container>
-      <img src={logo} alt="" />
+      <Link to="/" title="Coffee Delivery">
+        <img src={logo} alt="" />
+      </Link>
 
       <Aside>
         <CityContent>
@@ -19,7 +22,9 @@ export function Header() {
         </CityContent>
 
         <ShoppingCartContent>
-          <ShoppingCart size={22} weight="fill" color="#C47F17" />
+          <Link to="/checkout" title="Carrinho">
+            <ShoppingCart size={22} weight="fill" color="#C47F17" />
+          </Link>
         </ShoppingCartContent>
       </Aside>
     </Container>
