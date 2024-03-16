c22cfc0a5ccad6d510964a88db28dd63a9ef1f7f
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-03-15 20:16:03 -0500

    feat: add intro styled components for Home page
    The new Home page markup has been styled using the Styled Components library. Different sections like Intro, IntroDescription, Item, Items, and TextContainer have been defined with appropriate styling. Elements within the Home function have also been updated accordingly to reflect these changes.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,51 @@
+import {
+  Intro,
+  IntroDescription,
+  Item,
+  Items,
+  TextContainer,
+} from "./styles.ts";
+import { ShoppingCart } from "@phosphor-icons/react";
+import { useTheme } from "styled-components";
+import coffeeIntro from "../../assets/img/coffee-intro.png";
+
 export function Home() {
-  return <p>Home</p>;
+  const theme = useTheme();
+
+  return (
+    <Intro>
+      <IntroDescription>
+        <TextContainer>
+          <strong>Encontre o café perfeito para qualquer hora do dia</strong>
+          <p>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </p>
+        </TextContainer>
+
+        <Items>
+          <Item bg={theme["yellow-dark"]}>
+            <ShoppingCart color={theme["background"]} weight="fill" size={32} />
+            <span>Compra simple e segura</span>
+          </Item>
+          <Item bg={theme["base-text"]}>
+            <ShoppingCart color={theme["background"]} weight="fill" size={32} />
+            <span>Embalagem mantém o café intacto</span>
+          </Item>
+          <Item bg={theme["yellow"]}>
+            <ShoppingCart color={theme["background"]} weight="fill" size={32} />
+            <span>Entrega rápida e rastreada</span>
+          </Item>
+          <Item bg={theme["purple"]}>
+            <ShoppingCart color={theme["background"]} weight="fill" size={32} />
+            <span>O café chega fresquinho até você</span>
+          </Item>
+        </Items>
+      </IntroDescription>
+
+      <div>
+        <img src={coffeeIntro} alt="" />
+      </div>
+    </Intro>
+  );
 }
