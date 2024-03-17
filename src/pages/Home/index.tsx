149b06e3a76089e7187556bf636e3535822923f7
Marcos Santos <89700349+marcos-nsantos@users.noreply.github.com>
2024-03-17 10:50:42 -0500

    feat: add coffee product section to home page
    - Styled components have been added to the Home page's styles.ts file to create a visually engaging listing for coffee products.
    - The Home page has been updated with the new coffee product section. Each product item includes an image, tags, title, description, price, and a control for order quantity and addition to the shopping cart.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,69 @@
 import { IntroSection } from "./components/IntroSection";
+import cafeExpressoTradicional from "../../assets/img/coffee/caffee-expresso-tradicional.png";
+import {
+  CoffeeContainer,
+  CoffeeDescription,
+  CoffeeImage,
+  CoffeeItem,
+  CoffeeList,
+  CoffeeListTitle,
+  CoffeeTags,
+  CoffeeTitle,
+  Control,
+  Order,
+  Price,
+  QuantityInput,
+} from "./styles.ts";
+import { Minus, Plus, ShoppingCart } from "@phosphor-icons/react";
+import { useTheme } from "styled-components";
 
 export function Home() {
+  const theme = useTheme();
+
   return (
     <main>
       <IntroSection />
+
+      <CoffeeContainer>
+        <CoffeeListTitle>Nossos cafés</CoffeeListTitle>
+
+        <CoffeeList>
+          <CoffeeItem>
+            <CoffeeImage src={cafeExpressoTradicional} />
+
+            <CoffeeTags>
+              <span>Tradicional</span>
+            </CoffeeTags>
+            <CoffeeTitle>Expresso Tradicional</CoffeeTitle>
+            <CoffeeDescription>
+              O tradicional café feito com água quente e grãos moídos
+            </CoffeeDescription>
+
+            <Control>
+              <Price>
+                <span>R$</span>
+                <span>9,99</span>
+              </Price>
+
+              <Order>
+                <QuantityInput>
+                  <button>
+                    <Minus size={14} />
+                  </button>
+                  <span>1</span>
+                  <button>
+                    <Plus size={14} />
+                  </button>
+                </QuantityInput>
+
+                <button>
+                  <ShoppingCart size={22} color={theme["base-card"]} />
+                </button>
+              </Order>
+            </Control>
+          </CoffeeItem>
+        </CoffeeList>
+      </CoffeeContainer>
     </main>
   );
 }
